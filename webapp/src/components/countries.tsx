e97921cef00a4c40a6e747850a6bc74229f4d2c7
dnprogramming <speedcomputersdn@gmail.com>
2023-12-18 01:16:42 -0500

    Add Form to add countries

--- webapp/src/components/countries.tsx
+++ webapp/src/components/countries.tsx
@@ -1,43 +1,66 @@
 import React, {useState, useEffect} from 'react';
 import {CountryClient} from '../generated/CountryServiceClientPb';
-import {CountryInformation, GetCountriesRequest} from '../generated/country_pb';
+import {
+  AddCountryRequest,
+  CountryInformation,
+  GetCountriesRequest,
+} from '../generated/country_pb';
 
 const CountriesList: React.FC = () => {
   const [countries, setCountries] = useState<CountryInformation[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [newCountry, setNewCountry] = useState('');
+  const client = new CountryClient('http://localhost:8080');
 
   useEffect(() => {
-    const getCountries = async () => {
-      try {
-        setLoading(true);
-
-        const client = new CountryClient('http://localhost:8080');
-
-        const request = new GetCountriesRequest();
-        const response = await client.getCountries(request, {});
-
-        setCountries(response.getCountriesList());
-        setLoading(false);
-      } catch (error: any) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     getCountries();
   }, []);
 
-  if (countries.length === 0) {
-    return <div>No Data</div>;
-  }
+  const getCountries = async () => {
+    const request = new GetCountriesRequest();
+    const response = await client.getCountries(request, {});
+    setCountries(response.getCountriesList());
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const request = new AddCountryRequest();
+
+      request.setCountryname(newCountry);
+      await client.addCountry(request);
+      getCountries();
+    } catch (error) {
+      console.error(error);
+      alert('Error submitting form!');
+    }
+  };
+
   return (
-    <ul>
-      {countries.map(country => (
-        <li key={country.getCountryid()}>{country.getCountryname()}</li>
-      ))}
-    </ul>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Country:
+          <input
+            type="text"
+            value={newCountry}
+            onChange={e => setNewCountry(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {countries.length > 0 && (
+        <ul>
+          {countries.map(country => (
+            <li key={country.getCountryid()}>{country.getCountryname()}</li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
