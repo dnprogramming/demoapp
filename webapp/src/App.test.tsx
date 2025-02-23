852fe42175eef82c5e76aeb4e66964a9ea909cae
dnprogramming <speedcomputersdn@gmail.com>
2025-02-23 18:48:15 -0500

    Fixing issue

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {expect, describe, test} from '@jest/globals';
-import {render, screen} from '@testing-library/react';
 import App from './App';
 
 describe('test', () => {
